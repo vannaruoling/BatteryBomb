8108e18ee39324ad84e3148f7f430d5b1f917f46
vindennt <truong.dennis00@gmail.com>
2026-07-24 10:36:10 -0700

    Fixed bug where enemy counter goes negative, triggering new rounds consecutively

--- BatteryBomb/Assets/Scripts/RoundManager.cs
+++ BatteryBomb/Assets/Scripts/RoundManager.cs
@@ -13,6 +13,7 @@ public class RoundManager : MonoBehaviour
     public float spawnInterval = 1.5f;
 
     private int enemiesAlive = 0;
+    private bool roundActive = false;
 
     void Awake()
     {
@@ -28,11 +29,12 @@ public class RoundManager : MonoBehaviour
         ResetGameBoard();
         GameManager.Instance.inputEnabled = true;
 
-
+        // Display HP
+        // TODO: Dont need this to fade in at the start of every round
         DamageFlashDisplay.Instance.ShowDamage(GameManager.Instance.playerHealth);
 
-
         enemiesAlive = enemiesPerRound;
+        roundActive = true;
         enemySpawner.SpawnWave(enemiesPerRound, spawnInterval);
     }
 
@@ -42,6 +44,13 @@ public class RoundManager : MonoBehaviour
         foreach (BatteryBomb b in bombs)
         {
             Destroy(b.gameObject);
+        }
+
+        // Destroy all enemies to prevent them from persisting
+        Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+        foreach (Enemy e in enemies)
+        {
+            Destroy(e.gameObject);
         }
 
         TurretBase[] turrets = FindObjectsByType<TurretBase>(FindObjectsSortMode.None);
@@ -54,10 +63,14 @@ public class RoundManager : MonoBehaviour
     // Call this from Enemy.Die()
     public void ReportEnemyDeath()
     {
+        // PRevents bug where deaths were reported between rounds
+        if (!roundActive) return;
+
         enemiesAlive--;
         Debug.Log("Enemy died, remaining: " + enemiesAlive);
         if (enemiesAlive <= 0)
         {
+            roundActive = false;
             Debug.Log("Calling end round");
             EndRound();
         }
